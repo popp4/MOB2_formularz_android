a38014e0cd40b4e257d0a582229118174d6f3b91
Piotr Stankiewicz <10376582u@qed.local>
2024-10-03 14:18:32 +0200

    Initial commit

---
+++ app/src/main/java/com/example/formvalidationapp/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.formvalidationapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPhone;
+    private EditText editTextPassword;
+    private EditText editTextSurname;
+    private EditText editTextPassword2;
+    private Button buttonSubmit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Powiązanie widoków z kodem
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+        editTextPassword2 = findViewById(R.id.editTextTextPassword2);
+        editTextSurname = findViewById(R.id.editTextSurname);
+
+        buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Pobranie wprowadzonych danych
+                String name = editTextName.getText().toString().trim();
+                String email = editTextEmail.getText().toString().trim();
+                String phone = editTextPhone.getText().toString().trim();
+                String password = editTextPassword.getText().toString().trim();
+                String password2 = editTextPassword2.getText().toString().trim();
+                String surname = editTextSurname.getText().toString().trim();
+
+
+                // Walidacja danych------------------------------
+
+                //IMIE
+                if (name.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę wprowadzić imię", Toast.LENGTH_SHORT).show();
+
+                //NUMER TELEFONU
+                } else if (phone.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę wprowadzić numer telefonu", Toast.LENGTH_SHORT).show();
+                } else if (phone.length()<9) {
+                    Toast.makeText(MainActivity.this, "Niepoprawny numer telefonu", Toast.LENGTH_SHORT).show();
+
+                //HASLO
+                } else if (password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę wprowadzić hasło", Toast.LENGTH_SHORT).show();
+                } else if (password.length()<6) {
+                    Toast.makeText(MainActivity.this, "Hasło musi posiadać co najmniej 6 liter", Toast.LENGTH_SHORT).show();
+
+                } else if (password2.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę powtórzyć hasło", Toast.LENGTH_SHORT).show();
+                } else if (!(password2.equals(password))) {
+                    Toast.makeText(MainActivity.this, "Hasło sie nie zgadza", Toast.LENGTH_SHORT).show();
+
+                    //NAZWISKO
+                } else if (surname.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę wprowadzić nazwisko", Toast.LENGTH_SHORT).show();
+
+                //EMAIL
+                } else if (email.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Proszę wprowadzić adres e-mail", Toast.LENGTH_SHORT).show();
+                } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(MainActivity.this, "Niepoprawny adres email", Toast.LENGTH_SHORT).show();
+
+                //PRZYCISK
+                } else {
+                    Toast.makeText(MainActivity.this, "Formularz przesłany poprawnie", Toast.LENGTH_SHORT).show();
+
+
+                }
+            }
+        });
+    }
+}
